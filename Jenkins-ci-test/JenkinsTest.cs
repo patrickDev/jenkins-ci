b30764a00f9f3e85132494ebb8f62657d8d68714
PatrickDev <patrick_dev@outlook.com>
2017-05-31 18:21:36 -0400

    first commit

---
+++ Jenkins-ci-test/JenkinsTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Jenkins_ci_test
+{
+    public  class JenkinsTest
+    {
+        [Fact]
+        public  void FirstTest()
+        {
+            Assert.True(true);
+        }
+
+
+
+        [Fact]
+        public void SecondTest()
+        {
+            Assert.True(false);
+        }
+
+    }
+}
